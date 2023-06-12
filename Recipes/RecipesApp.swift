6a867030f28867d0d1e3ea298d983a6be8e9c653
haytham odeh <haythamlion@outlook.com>
2023-06-12 16:22:21 -0700

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Haytham Odeh on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
